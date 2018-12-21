37231f2219a2f21ee736c3904d9645e2b6b0e986
mappingvermont <charlie.hofmann@arcadiapower.com>
2018-12-21 14:24:32 -0500

    implement older solution from chromium discussion board

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # headless-chrome-fail-case
 
-An MWE to show the limitations of headless chrome when downloading a PDF from a new tab link.
+An MWE to show the limitations of headless chrome when downloading a PDF from a new tab link. The options and preferences used to configure chrome were taken from [this chromium thread](https://bugs.chromium.org/p/chromium/issues/detail?id=696481#c89). These comments are old, but it's the only one to seem to have gotten things working with capybara.
 
 ### Installation
 
--- scrapppy.rb
+++ scrapppy.rb
@@ -7,66 +7,40 @@ require 'pry'
 
 DOWNLOAD_PATH = Pathname.new('headless-downloads')
 USER_DIR = Pathname.new('chrome-dir')
-SWITCHES = [
-  '--window-size=1280,1024',
-  '--disable-bundled-ppapi-flash',
-  '--disable-extensions',
-  '--disable-java',
-  '--disable-plugins',
-  '--disable-plugins-discovery',
-  '--ignore-certificate-errors',
-  '--headless',
-  '--disable-gpu',
-  '--incognito',
-  '--new-window',
-  '--no-sandbox',
-  '--disable-dev-shm-usage',
-  "--user-data-dir=#{USER_DIR}"
-]
-def chrome_options
-  Selenium::WebDriver::Chrome::Options.new.tap do |options|
-    options.add_preference('pdfjs.disabled', true)
-    options.add_preference('plugins.always_open_pdf_externally', true)
-    options.add_preference(
-      'plugins.plugins_disabled',
-      [
-        'Chrome PDF Viewer',
-        'Adobe Flash Player',
-        'Widevine Content Decryption Module',
-        'Native Client'
-      ]
-    )
-
-    options.add_preference(
-      :download,
-      directory_upgrade: true,
-      prompt_for_download: false,
-      default_directory: DOWNLOAD_PATH
-    )
-    options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
-
-    SWITCHES.each do |switch|
-      options.add_argument(switch)
-    end
-  end
-end
 
 def configure
   FileUtils.rm_rf(DOWNLOAD_PATH)
 
-  Capybara::Driver::Base.class_eval { def status_code; 'unknown'.freeze; end }
-  Capybara.default_max_wait_time = 10
-
   Capybara.register_driver :headless do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options).tap do |driver|
-      driver.browser.download_path = DOWNLOAD_PATH
-    end
+    options = Selenium::WebDriver::Chrome::Options.new
+  
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--window-size=1366,768')
+  
+    options.add_preference(:download, directory_upgrade: true,
+                                      prompt_for_download: false,
+                                      default_directory: DOWNLOAD_PATH)
+  
+    options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
+  
+    driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  
+    bridge = driver.browser.send(:bridge)
+  
+    path = '/session/:session_id/chromium/send_command'
+    path[':session_id'] = bridge.session_id
+  
+    bridge.http.call(:post, path, cmd: 'Page.setDownloadBehavior',
+                                  params: {
+                                    behavior: 'allow',
+                                    downloadPath: DOWNLOAD_PATH
+                                  })
+  
+    driver
   end
-
-  Capybara.default_driver = :headless
-  Capybara.javascript_driver = :headless
-  Capybara.current_driver = :headless
-  Selenium::WebDriver.logger.level = :warn
 end
 
 def list_files

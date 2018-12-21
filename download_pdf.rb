70b9f26073617de59bdf32153236e81842e85c9d
Ryan Sandridge <noreply@ryansandridge.com>
2018-12-21 14:46:02 -0500

    Make the demo do both links.

--- download_pdf.rb
+++ download_pdf.rb
@@ -5,8 +5,17 @@ session = Capybara::Session.new(:headless)
 
 list_files
 
+puts "First click on a newtab_link"
 session.visit "http://localhost:8000/"
 link_node = session.find_link(id: "newtab_link")
+puts "Clicking link"
+link_node.click
+puts "Waiting a few seconds"
+sleep 3
+list_files
+
+puts "Now click on a normal link"
+link_node = session.find_link(id: "normal_link")
 puts "Clicking link"
 link_node.click
 puts "Waiting a few seconds"

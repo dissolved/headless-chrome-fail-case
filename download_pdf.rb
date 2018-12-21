8ed35ab5e530640012ac585713f3fd9cf6c82afa
mappingvermont <charlie.hofmann@arcadiapower.com>
2018-12-21 14:54:45 -0500

    remove all target=_blank attributes from links

--- download_pdf.rb
+++ download_pdf.rb
@@ -6,6 +6,13 @@ session = Capybara::Session.new(:headless)
 list_files
 
 session.visit "http://localhost:8000/"
+
+# https://stackoverflow.com/a/10098904/4355916
+session.execute_script(%Q(
+   Array.from(document.querySelectorAll('a[target="_blank"]'))
+    .forEach(link => link.removeAttribute('target'));
+   ))
+
 link_node = session.find_link(id: "newtab_link")
 puts "Clicking link"
 link_node.click
